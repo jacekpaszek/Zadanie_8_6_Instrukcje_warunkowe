c9325907fa4db0794677442f26c2c491efb59850
Jacek <jacek.paszek@onet.pl>
2017-06-16 23:49:53 +0200

    syntax modification

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-var a = prompt('Enter value a'),
-    b = prompt('Enter value b');
+var a = Number(prompt('Enter value a')),
+    b = Number(prompt('Enter value b'));
 
-if (Number.isNaN(Number(a)) || Number.isNaN(Number(b))) {
+if (Number.isNaN(a) || Number.isNaN(b)) {
   console.log('Equation parameters are wrong'); //equation parameters checking
-  } else {
+} else {
   var value = (a * a) + (2 * a * b) - (b * b); //equation calculation
   console.log(
-  	value === 0 ? 'Wynik jest równy zero' : (value < 0 ? 'Wynik jest ujemny' 
-  	: 'Wynik jest dodatni')
+  	value === 0 
+  	  ? 'Wynik jest równy zero'
+  	  : (value < 0 ? 'Wynik jest ujemny' : 'Wynik jest dodatni')
   );
 }
